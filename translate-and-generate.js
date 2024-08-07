3e4ea838c717524db592b60caacc77a1c59c5138
darren <darren4ten@163.com>
2024-08-07 11:58:14 +0800

    update

--- translate-and-generate.js
+++ translate-and-generate.js
@@ -40,6 +40,7 @@ async function translateNews(newsListPath) {
       console.log(`Translated article saved to ${newsItem.id}`);
       // 提交生成的文件到 GitHub 仓库的 news 目录下
       const commitMessage = `Add translated article ${filename}`;
+      execSync(`-c "user.name=darren4ten" -c "user.email=darren4ten@163.com"`);
       execSync(`git add ${filename}`);
       execSync(`git commit -m "${commitMessage}"`);
       execSync(`git push origin HEAD`);
